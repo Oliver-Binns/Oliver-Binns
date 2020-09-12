e6931a3eaa17018c01343c4d6c7d00ac0b750658
Oliver Binns <mail@oliverbinns.co.uk>
2020-09-12 12:12:15 +0100

    Initial Commit

---
+++ Oliver Binns/Oliver_BinnsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Oliver_BinnsApp.swift
+//  Oliver Binns
+//
+//  Created by Laptop 3 on 12/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Oliver_BinnsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
