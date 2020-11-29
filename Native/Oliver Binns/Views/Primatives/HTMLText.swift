50aacb89067b279a74858882f4a59094b4f5c7e5
Oliver Binns <mail@oliverbinns.co.uk>
2020-11-29 16:49:42 +0000

    Use UITextField instead of UILabel

--- Native/Oliver Binns/Views/Primatives/HTMLText.swift
+++ Native/Oliver Binns/Views/Primatives/HTMLText.swift
@@ -46,7 +46,8 @@ fileprivate struct HTMLText: UIViewRepresentable {
             .addingAttributes([.foregroundColor: UIColor.label])
             .untilPhrase("…")
             .trimTrailingWhiteSpace()
-        uiView.setLinkColor(.accent)
+        uiView.tintColor = .accent
+        uiView.isEditable = false
 
         DispatchQueue.main.async {
             let size = uiView.intrinsicContentSize
@@ -55,77 +56,17 @@ fileprivate struct HTMLText: UIViewRepresentable {
         }
     }
 }
-fileprivate final class HTMLLabel: UILabel {
+fileprivate final class HTMLLabel: UITextView {
     var linkPressed: ((URL) -> Void)?
     
     private var attributedTextCopy: NSAttributedString?
 
-    lazy var textContainer: NSTextContainer = {
-        let textContainer = NSTextContainer(size: .zero)
-        textContainer.lineFragmentPadding = 0
-        textContainer.lineBreakMode = lineBreakMode
-        textContainer.maximumNumberOfLines = numberOfLines
-        textContainer.size = bounds.size
-        return textContainer
-    }()
-
     convenience init() {
         self.init(frame: .zero)
-        numberOfLines = 0
+        delegate = self
+        textContainerInset = .zero
+        textContainer.lineFragmentPadding = 0
         setContentCompressionResistancePriority(.defaultLow, for: .horizontal)
-        isUserInteractionEnabled = true
-
-        let gesture = UILongPressGestureRecognizer(target: self, action: #selector(handleGesture(gesture:)))
-        gesture.cancelsTouchesInView = false
-        gesture.delegate = self
-        gesture.allowableMovement = 0.5
-        gesture.minimumPressDuration = 0
-        self.addGestureRecognizer(gesture)
-    }
-
-    @objc private func handleGesture(gesture: UILongPressGestureRecognizer) {
-        switch gesture.state {
-        case .began:
-            onTouchDown(tapGesture: gesture)
-        case .ended:
-            onTouchUp(tapGesture: gesture)
-        default:
-            setLinkColor(.accent)
-        }
-    }
-
-    private func onTouchDown(tapGesture: UIGestureRecognizer) {
-        let tapPoint = tapGesture.location(in: self)
-        guard let characterIndex = characterTapped(at: tapPoint) else {
-            return
-        }
-        setLinkColor(.gray, at: characterIndex)
-    }
-
-    private func onTouchUp(tapGesture: UIGestureRecognizer) {
-        let tapPoint = tapGesture.location(in: self)
-        guard let characterIndex = characterTapped(at: tapPoint),
-              let linkValue = attributedText?.attribute(.link, at: characterIndex, effectiveRange: nil) as? URL else {
-            return
-        }
-        linkPressed?(linkValue)
-        setLinkColor(.accent)
-    }
-
-    public func setLinkColor(_ color: UIColor, at characterIndex: Int? = nil) {
-        guard let attributedText = attributedText else { return }
-        attributedText.enumerateAttribute(.link,
-                                          in: NSRange(location: 0, length: attributedText.length)) { (value, range, _) in
-            guard value != nil else { return }
-            if let characterIndex = characterIndex,
-               !range.contains(characterIndex) {
-                return
-            }
-            let string = NSMutableAttributedString(attributedString: attributedText)
-            string.removeAttribute(.foregroundColor, range: range)
-            string.addAttribute(.foregroundColor, value: color.cgColor, range: range)
-            self.attributedText = string
-         }
     }
 
     override var intrinsicContentSize: CGSize {
@@ -139,8 +80,8 @@ fileprivate final class HTMLLabel: UILabel {
         guard let attributedText = attributedText else { return nil }
         let layoutManager = NSLayoutManager()
         let textStorage = NSTextStorage(attributedString: attributedText)
-        layoutManager.addTextContainer(textContainer)
         textStorage.addLayoutManager(layoutManager)
+        layoutManager.addTextContainer(textContainer)
 
         let textBoundingBox = layoutManager.usedRect(for: textContainer)
         let textContainerOffset = CGPoint(x: (bounds.size.width - textBoundingBox.size.width) * 0.5 - textBoundingBox.origin.x,
@@ -150,14 +91,13 @@ fileprivate final class HTMLLabel: UILabel {
         return layoutManager.characterIndex(for: locationOfTouchInTextContainer, in: textContainer, fractionOfDistanceBetweenInsertionPoints: nil)
     }
 }
-extension HTMLLabel: UIGestureRecognizerDelegate {
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-                           shouldRecognizeSimultaneouslyWith otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        true
-    }
-
-    func gestureRecognizer(_ gestureRecognizer: UIGestureRecognizer,
-                           shouldRequireFailureOf otherGestureRecognizer: UIGestureRecognizer) -> Bool {
-        false
+extension HTMLLabel: UITextViewDelegate {
+    func textView(_ textView: UITextView,
+                  shouldInteractWith URL: URL,
+                  in characterRange: NSRange,
+                  interaction: UITextItemInteraction) -> Bool {
+        
+        linkPressed?(URL)
+        return false
     }
 }
